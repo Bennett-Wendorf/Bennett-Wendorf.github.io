aa969851e0a74f45287e4f751c3ed07300762314
Bennett-Wendorf <bennettwendorf@yahoo.com>
2019-08-14 19:53:24 -0500

    Played with live server extension in vs code

--- Script.js
+++ Script.js
@@ -1,4 +1,7 @@
 console.log("Script.js connected!");
 function changePara() {
-    document.getElementById("demo").innerHTML = "Paragraph changed.";
+    document.getElementById("demo").innerHTML = "Paragraph not changed.";
+}
+function changeParaBack() {
+    document.getElementById("demo").innerHTML = "This is a new paragraph!";
 }
